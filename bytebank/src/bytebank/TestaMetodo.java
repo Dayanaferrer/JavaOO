e18c963726b1eee2b2c85dd45e7c7ce39e82e496
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-01-30 12:30:55 -0300

    Adicionei saque ao programa.

--- bytebank/src/bytebank/TestaMetodo.java
+++ bytebank/src/bytebank/TestaMetodo.java
@@ -8,6 +8,12 @@ public class TestaMetodo {
 		contaDaDay.deposita(50);
 		System.out.println(contaDaDay.saldo);
 		
+		boolean conseguiuRetirar = contaDaDay.saca (20);
+		System.out.println(contaDaDay.saldo);
+		System.out.println(conseguiuRetirar);
+		
+		
+		
 
 	}
 
