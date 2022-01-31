971e684651eeb545b7a37d23ca3fb9a2502a7a48
Dayana Ferreira <53124488+Dayanaferrer@users.noreply.github.com>
2022-01-31 11:19:33 -0300

    Finish commit

--- byte-bank-encapsulado/src/TestaValores.java
+++ byte-bank-encapsulado/src/TestaValores.java
@@ -5,6 +5,13 @@ public class TestaValores {
 		Conta conta = new Conta(1337, 24226);
 		
 		System.out.println("Agência da conta criada: " + conta.getAgencia());
+		
+		conta.setAgencia(123123123);
+		
+		Conta conta2 = new Conta(1337, 16549);
+		Conta conta3 = new Conta(1337, 696969);
+		
+		System.out.println(Conta.getTotal());
 
 	}
 
